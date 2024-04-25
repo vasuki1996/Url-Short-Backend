7fcb9f3274ac9cfe0a9e8a1bbebd62b58c242b27
Vasuki Vardhan <vasukivardhan96@gmail.com>
2024-04-25 17:07:42 +0530

    Update env.js

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 module.exports = {
-	mongo_url : "mongodb+srv://vasukivardhan:Vasu@1996@cluster0-qruel.mongodb.net/UrlShortNew?retryWrites=true&w=majority",
-	redis_host : "redis-16074.c114.us-east-1-4.ec2.cloud.redislabs.com",
+	mongo_url : "",
+	redis_host : "",
 	redis_port : 16074,
-	redis_password: "V49AuEQ1RjxAIM91YVipLDL4bonYv0Dq"
+	redis_password: ""
 }
